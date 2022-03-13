52f705cae58b0a107c73f84590cc6cf9233cdfbe
haruto17 <proaca157@gmail.com>
2022-03-13 23:03:56 +0900

    add 101-a.cpp

---
+++ AtCoder_saimarooooooo/101-a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int ans = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		char c;
+		cin >> c;
+
+		if (c == '+')ans++;
+		else ans--;
+	}
+
+	cout << ans << endl;
+}
