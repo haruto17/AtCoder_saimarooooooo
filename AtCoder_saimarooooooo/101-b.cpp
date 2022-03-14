2a79a21174b984286515f0e091863ee5368d3a5f
haruto17 <proaca157@gmail.com>
2022-03-14 22:16:06 +0900

    add 101-b

---
+++ AtCoder_saimarooooooo/101-b.cpp
@@ -0,0 +1,23 @@
+//101-b
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	cin >> S;
+
+	int sum = 0, N = stoi(S);
+
+	for (int i = 0; i < S.length(); i++)
+	{
+		char c = S[i];
+		sum += int(c - '0');
+	}
+
+	if (N % sum == 0)cout << "Yes" << endl;
+	else cout << "No" << endl;
+}
