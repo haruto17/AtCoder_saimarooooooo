89dfcf985356c6f71678e6248ed9ce564108fbc7
haruto17 <proaca157@gmail.com>
2022-03-16 18:07:04 +0900

    add 104-b.cpp(ARC)

---
+++ AtCoder_saimarooooooo/104-a.cpp
@@ -0,0 +1,18 @@
+//ARC104-b
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int A, B;
+	cin >> A >> B;
+	
+	int X = (A + B) / 2;
+	int Y = (A - B) / 2;
+
+	cout << X << " " << Y << endl;
+
+	return 0;
+}
