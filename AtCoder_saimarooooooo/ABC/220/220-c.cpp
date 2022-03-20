d852cc60c3ef7f90e1eccb918716fca2d11944d8
haruto17 <50482005+haruto17@users.noreply.github.com>
2022-03-20 18:54:46 +0900

    Create 220-c.cpp
    AtCoder Beginner Contest 220
    C - Long Sequence

---
+++ AtCoder_saimarooooooo/ABC/220/220-c.cpp
@@ -0,0 +1,42 @@
+//ABC220-c
+
+#include<iostream>
+#include<vector>
+#include<numeric>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+	ll N;
+	cin >> N;
+	
+	vector<ll> A(N);
+	for (int i = 0; i < N; i++)cin >> A[i];
+
+	ll X;
+	cin >> X;
+
+	ll sum = accumulate(A.begin(), A.end(), 0ll);
+	ll P = X / sum;
+
+	//Ø‚èã‚°
+	ll sumb = P * sum;
+
+	ll ans = P * N;
+
+	for (ll val : A)
+	{
+		sumb += val;
+		ans++;
+
+		if (sumb > X)
+		{
+			cout << ans << endl;
+			return 0;
+		}
+	}
+
+	return 0;
+}
