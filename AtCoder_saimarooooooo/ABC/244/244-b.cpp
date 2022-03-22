ed8bc72acca8331eeb28a571f9f16aaf00089b57
haruto17 <50482005+haruto17@users.noreply.github.com>
2022-03-22 11:36:40 +0900

    Create 244-b.cpp
    AtCoder Beginner Contest 244
    B - Go Straight and Turn Right

---
+++ AtCoder_saimarooooooo/ABC/244/244-b.cpp
@@ -0,0 +1,39 @@
+//ABC244-b
+
+#include<iostream>
+
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (int)(n);i++)
+
+int main()
+{
+	int N;
+	cin >> N;
+	string T;
+	cin >> T;
+
+	int direction = 1;
+	int ans_x = 0;
+	int ans_y = 0;
+
+	rep(i, T.length())
+	{
+		if (T[i] == 'S')
+		{
+			if (direction == 1)ans_x++;
+			else if (direction == 2)ans_y--;
+			else if (direction == 3) ans_x--;
+			else if (direction == 4)ans_y++;
+		}
+		else if (T[i] == 'R')
+		{
+			direction++;
+			if (direction > 4)direction = 1;
+		}
+	}
+
+	cout << ans_x << " " << ans_y << endl;
+
+	return 0;
+}
