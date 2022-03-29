9b1ec348c718e21cc1654c2e880c96f0a9265b8f
haruto17 <50482005+haruto17@users.noreply.github.com>
2022-03-29 12:39:47 +0900

    Update 002-c.cpp
    Change calculation method

--- AtCoder_saimarooooooo/ABC/002/002-c.cpp
+++ AtCoder_saimarooooooo/ABC/002/002-c.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 int main()
 {
-	double x1, x2, x3;
-	double y1, y2, y3;
-	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+	double a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
 
-	double a = x1 * y2 + x2 * y3 + x3 * y1 - y1 * x2 - y2 * x3 - y3 * x1;
-	double S = abs(a / 2);
+	double q = (c - a) * (f - b) - (d - b) * (e - a);
+	double S = abs(q / 2);
+
 
 	cout << fixed << setprecision(1) << S << endl;
 
