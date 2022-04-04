0e53c5813d428973b575dbb4a77d112f686c800e
haruto17 <50482005+haruto17@users.noreply.github.com>
2022-04-04 09:44:03 +0900

    Update 245-a.cpp

--- AtCoder_saimarooooooo/ABC/245/245-a.cpp
+++ AtCoder_saimarooooooo/ABC/245/245-a.cpp
@@ -9,14 +9,7 @@ int main()
 	int a, b, c, d;
 	cin >> a >> b >> c >> d;
 
-	if (a == c)
-	{
-		if (b == d)cout << "Takahashi" << endl;
-		else if (b < d) cout << "Takahashi" << endl;
-		else if (d < b)cout << "Aoki" << endl;
-	}
-	else if (a < c)cout << "Takahashi" << endl;
-	else if (c < a)cout << "Aoki" << endl;
+	cout << (make_pair(a, b) <= make_pair(c, d) ? "Takahashi" : "Aoki") << endl;
 
 	return 0;
 }
