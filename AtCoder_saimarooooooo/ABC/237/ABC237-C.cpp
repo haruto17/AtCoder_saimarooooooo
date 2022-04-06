c5b69b2090cf9a6b1fdf604a094b44828cfdaf18
haruto17 <50482005+haruto17@users.noreply.github.com>
2022-04-06 20:14:16 +0900

    Update ABC237-C.cpp

--- AtCoder_saimarooooooo/ABC/237/ABC237-C.cpp
+++ AtCoder_saimarooooooo/ABC/237/ABC237-C.cpp
@@ -1,71 +1,35 @@
 //ABC237-C
-//WA
+//AC
 
 #include<iostream>
-#include<string>
-#include<algorithm>
 
 using namespace std;
-
-#define all(x) begin(x),end(x)
-
-string erase_a(string x)
-{
-	while (x.back() == 'a')
-	{
-		x.pop_back();
-	}
-	return x;
-}
-
-bool check(string x)
-{
-	while (x.size() > 1)
-	{
-		if (x.front() != x.back())
-		{
-			return false;
-		}
-
-		x.erase(0, 1);
-		x.erase(x.size() - 1, 1);
-	}
-
-	return true;
-}
 
 int main()
 {
 	string s;
 	cin >> s;
 
-	string rev_s = s;
-	reverse(all(rev_s));
+	int n = s.size();
+	int l = 0, r = n - 1;
 
-	if (s == rev_s)
+	//文字列の両端がaのとき、カーソルを進める
+	while (l < r && s[l] == 'a' && s[r] == 'a')
 	{
-		cout << "Yes" << endl;
-		return 0;
+		++l, --r;
 	}
+
+	//文字列の右端だけがaのとき,カーソルを進める
+	while (l < r && s[r] == 'a')--r;
 	
-	if (s.back() != 'a')
+	//両端が同じ文字のとき、カーソルを進める
+	while (l < r && s[l] == s[r])
 	{
-		cout << "No" << endl;
-		return 0;
+		++l, --r;
 	}
-	else if (s.back() == 'a')
-	{
-		string no_a = erase_a(s);
-		
-		if (check(no_a))
-		{
-			cout << "Yes" << endl;
-			return 0;
-		}
-		else if (!check(no_a))
-		{
-			cout << "No" << endl;
-			return 0;
-		}
-	}
+
+	if (l >= r) cout << "Yes" << endl;
+	else cout << "No" << endl;
+
+	return 0;
 }
