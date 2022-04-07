b675d2e70b61fa3ab808e4c76682e408ec3ce6d7
haruto17 <50482005+haruto17@users.noreply.github.com>
2022-04-07 18:36:32 +0900

    Update ABC234-B.cpp

--- AtCoder_saimarooooooo/ABC/234/ABC234-B.cpp
+++ AtCoder_saimarooooooo/ABC/234/ABC234-B.cpp
@@ -9,24 +9,6 @@ using namespace std;
 
 #define rep(i,a,b) for(int i = a;i < b;i++)
 
-double calc(int a,vector<int>&x, vector<int>&y)
-{
-	double ans = 0;
-
-	rep(i, 0, a)
-	{
-		rep(j, i, a)
-		{
-			double d_x = x[j] - x[i];
-			double d_y = y[j] - y[i];
-			double sum = d_x * d_x + d_y * d_y;
-			ans = max(ans, sqrt(sum));
-		}
-	}
-
-	return ans;
-}
-
 int main()
 {
 	int n;
@@ -35,7 +17,20 @@ int main()
 	vector<int> x(n), y(n);
 	rep(i, 0, n) cin >> x[i] >> y[i];
 
-	cout << fixed << setprecision(10) << calc(n, x, y) << endl;
+	double ans = 0;
+
+	rep(i, 0, n)
+	{
+		rep(j, i, n)
+		{
+			double d_x = x[j] - x[i];
+			double d_y = y[j] - y[i];
+			double sum = d_x * d_x + d_y * d_y;
+			ans = max(ans, sqrt(sum));
+		}
+	}
+
+	cout << fixed << setprecision(10) << ans << endl;
 
 	return 0;
 }
