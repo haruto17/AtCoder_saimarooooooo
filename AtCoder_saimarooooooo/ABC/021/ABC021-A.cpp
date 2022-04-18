ea3d0efaac4ced498528bde5457a1a22c6c346f3
haruto17 <50482005+haruto17@users.noreply.github.com>
2022-04-18 11:12:56 +0900

    Create ABC021-A.cpp
    AtCoder Beginner Contest 021
    A - 足し算

---
+++ AtCoder_saimarooooooo/ABC/021/ABC021-A.cpp
@@ -0,0 +1,17 @@
+//ABC021-A
+
+#include<iostream>
+
+using namespace std;
+#define rep(i,n) for(int i = 0; i < n;i++)
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << n << endl;
+	rep(i, n)cout << 1 << endl;
+
+	return 0;
+}
