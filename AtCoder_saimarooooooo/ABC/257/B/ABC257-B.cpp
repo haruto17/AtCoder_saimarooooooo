0db933748b608be6b82ebd0d4468316b8fa48163
haruto17 <s1902272@sendai-nct.jp>
2022-07-14 11:06:05 +0900

    change the ABC257-B.cpp

--- AtCoder_saimarooooooo/ABC/257/B/ABC257-B.cpp
+++ AtCoder_saimarooooooo/ABC/257/B/ABC257-B.cpp
@@ -11,8 +11,10 @@ int main()
     int n, k, q;
     cin >> n >> k >> q;
 
+    // n個のマス目　0ですべての要素を初期化
     vector<int> data(n, 0);
 
+    // Aの入力　コマがあるマスを0→1にする
     for (int i = 0; i < k; i++)
     {
         int a;
@@ -21,13 +23,20 @@ int main()
         data[a] = 1;
     }
 
+    // Lの入力
     for (int i = 0; i < q; i++)
     {
         int L;
         cin >> L;
+
+        //左からi番目のコマを探す
         int count = 0;
         for (int j = 0; j < data.size(); j++)
         {
+
+            //マスにコマが置いてあったら、countに加算する
+            // countとLiが一致したら、コマが動かせるか判定する
+
             if (data[j])
             {
                 count++;
@@ -35,6 +44,7 @@ int main()
 
             if (L == count)
             {
+                //判定
                 if (j != n - 1 && !data[j + 1])
                 {
                     swap(data[j], data[j + 1]);
@@ -43,6 +53,7 @@ int main()
         }
     }
 
+    //出力
     for (int i = 0; i < data.size(); i++)
     {
         if (data[i])
