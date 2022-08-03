c4446b4c3a3ed38b73e1e11530ea653683e40683
haruto17 <s1902272@sendai-nct.jp>
2022-08-03 15:33:21 +0900

    Add C.cpp ABC028

---
+++ AtCoder_saimarooooooo/ABC/028/C/C.cpp
@@ -0,0 +1,16 @@
+// ABC028-C
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+
+    cout << max(a + d + e, b + c + e) << endl;
+
+    return 0;
+}
