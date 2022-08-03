6edf64a85ac3f73b4235303979d84a43e9bc6eb9
haruto17 <s1902272@sendai-nct.jp>
2022-08-03 15:41:57 +0900

    Add A.cpp ABC011-A

---
+++ AtCoder_saimarooooooo/ABC/011/A/A.cpp
@@ -0,0 +1,22 @@
+// ABC011-A
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n == 12)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << n + 1 << endl;
+    }
+
+    return 0;
+}
