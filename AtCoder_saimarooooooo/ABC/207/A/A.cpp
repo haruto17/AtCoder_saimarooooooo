c2ffd7b38ccb2b46878afae8f0e8cf46723e015b
haruto17 <s1902272@sendai-nct.jp>
2022-08-11 14:50:43 +0900

    Add A.cpp ABC207

---
+++ AtCoder_saimarooooooo/ABC/207/A/A.cpp
@@ -0,0 +1,16 @@
+// ABC207
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << max({a, b, c}) + ((a + b + c) - (max({a, b, c}) + min({a, b, c}))) << endl;
+
+    return 0;
+}
