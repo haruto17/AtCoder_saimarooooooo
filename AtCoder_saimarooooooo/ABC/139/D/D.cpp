574de5da3e76dec2cb447d39678db6c4583457dc
haruto17 <s1902272@sendai-nct.jp>
2022-09-23 21:34:23 +0900

    add D.cpp (ABC139)

---
+++ AtCoder_saimarooooooo/ABC/139/D/D.cpp
@@ -0,0 +1,16 @@
+// ABC139-D
+
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ll N;
+    cin >> N;
+
+    cout << N * (N - 1) / 2 << endl;
+
+    return 0;
+}
