5707d14a3c43b9e379795af60f23a8adb7b2a343
haruto17 <s1902272@sendai-nct.jp>
2022-09-23 21:03:52 +0900

    add A.cpp (ABC139)

---
+++ AtCoder_saimarooooooo/ABC/139/A/A.cpp
@@ -0,0 +1,23 @@
+// ABC139-A
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string S, T;
+    cin >> S >> T;
+
+    int ans = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (S[i] == T[i])
+            ans++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
