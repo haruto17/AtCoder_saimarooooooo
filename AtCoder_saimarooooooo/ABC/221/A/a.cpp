0e8fc43c9bdef0eddb7fb3e7b25f56ae65791e87
haruto17 <s1902272@sendai-nct.jp>
2022-10-03 21:41:34 +0900

    add a.cpp (abc221)

---
+++ AtCoder_saimarooooooo/ABC/221/A/a.cpp
@@ -0,0 +1,16 @@
+// abc221-a
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << (int)pow(32, a - b) << endl;
+
+    return 0;
+}
