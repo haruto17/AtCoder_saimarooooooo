1832adcfe81aac0196dff644dfad823d24c10b01
haruto17 <s1902272@sendai-nct.jp>
2022-10-04 12:42:16 +0900

    add a.cpp (abc238)

---
+++ AtCoder_saimarooooooo/ABC/238/A/a.cpp
@@ -0,0 +1,22 @@
+// abc238-a
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n >= 2 && n <= 4)
+    {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout << "Yes" << endl;
+    }
+
+    return 0;
+}
