c279a15646f8dca2b61df93d4adee0df4369a89d
haruto17 <s1902272@sendai-nct.jp>
2022-10-04 12:32:08 +0900

    add a.cpp (abc229)

---
+++ AtCoder_saimarooooooo/ABC/229/A/a.cpp
@@ -0,0 +1,26 @@
+// abc229-a
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    if (s1 == "#." && s2 == ".#")
+    {
+        cout << "No" << endl;
+    }
+    else if (s1 == ".#" && s2 == "#.")
+    {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout << "Yes" << endl;
+    }
+
+    return 0;
+}
