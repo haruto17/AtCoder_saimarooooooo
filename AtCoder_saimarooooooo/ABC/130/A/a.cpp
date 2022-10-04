26f4ef0f7eb74a765f2bbe06ad5cd9f41040e8e7
haruto17 <s1902272@sendai-nct.jp>
2022-10-04 13:28:57 +0900

    add a.cpp (abc130)

---
+++ AtCoder_saimarooooooo/ABC/130/A/a.cpp
@@ -0,0 +1,22 @@
+// abc130-a
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, a;
+    cin >> x >> a;
+
+    if (x < a)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        cout << 10 << endl;
+    }
+
+    return 0;
+}
