e195f57b3998c2cd90cd83fee550af7051dedbc6
haruto17 <s1902272@sendai-nct.jp>
2022-10-06 12:28:07 +0900

    add a.cpp (abc269)

---
+++ AtCoder_saimarooooooo/ABC/269/A/a.cpp
@@ -0,0 +1,16 @@
+// abc269-a
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << (a + b) * (c - d) << endl;
+    cout << "Takahashi" << endl;
+
+    return 0;
+}
