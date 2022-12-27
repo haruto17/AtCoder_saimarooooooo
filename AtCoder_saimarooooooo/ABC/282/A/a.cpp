ef5e3092cff48a6f7533e586c2fced9d31f4c851
haruto17 <s1902272@sendai-nct.jp>
2022-12-27 22:06:25 +0900

    add abc282-a

---
+++ AtCoder_saimarooooooo/ABC/282/A/a.cpp
@@ -0,0 +1,25 @@
+// abc282-a
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    int k;
+    cin >> k;
+
+    for(int i = 0; i < k; i++) {
+        cout << char('A' + i);
+    }
+
+    return 0;
+}
