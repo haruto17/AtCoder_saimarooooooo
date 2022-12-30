bea2c1c975f67ab4b43adf0f7823a0966205603f
haruto17 <s1902272@sendai-nct.jp>
2022-12-30 17:37:05 +0900

    add abc261-a

---
+++ AtCoder_saimarooooooo/ABC/261/A/a.cpp
@@ -0,0 +1,28 @@
+// abc261-a
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int L1, R1, L2, R2;
+    cin >> L1 >> R1 >> L2 >> R2;
+
+    if(R2 <= L1 || R1 <= L2) {
+        cout << 0 << endl;
+    } else if(L2 >= L1 && R2 <= R1) {
+        cout << R2 - L2 << endl;
+    } else if(L1 >= L2 && R1 <= R2) {
+        cout << R1 - L1 << endl;
+    } else if((L2 >= L1 && L2 < R1) && R2 >= R1) {
+        cout << R1 - L2 << endl;
+    } else if((L1 >= L2 && L1 < R2) && R1 >= R2) {
+        cout << R2 - L1 << endl;
+    } else if((R2 <= R1 && R2 > L1) && L2 <= L1) {
+        cout << R2 - L1 << endl;
+    } else if((R1 <= R2 && R1 > L2) && L1 <= L2) {
+        cout << R1 - L2 << endl;
+    }
+
+    return 0;
+}
