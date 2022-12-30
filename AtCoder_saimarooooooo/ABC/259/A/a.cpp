4dc3dceb8ceb5997eca8f97827ac9970607a084a
haruto17 <s1902272@sendai-nct.jp>
2022-12-30 17:18:18 +0900

    add abc259-a

---
+++ AtCoder_saimarooooooo/ABC/259/A/a.cpp
@@ -0,0 +1,17 @@
+// abc259-a
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m, x, t, d;
+    cin >> n >> m >> x >> t >> d;
+
+    if(m >= x) {
+        cout << t << endl;
+    } else
+        cout << t - (x - m) * d << endl;
+
+    return 0;
+}
