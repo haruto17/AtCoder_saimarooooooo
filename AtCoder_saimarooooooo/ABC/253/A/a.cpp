0d504dfcdb14e030dd6e7ea3a99133f9067c797c
haruto17 <s1902272@sendai-nct.jp>
2022-12-30 17:06:35 +0900

    change abc253-a

--- AtCoder_saimarooooooo/ABC/253/A/a.cpp
+++ AtCoder_saimarooooooo/ABC/253/A/a.cpp
@@ -1,23 +1,39 @@
 // abc253-a
 
-#include <algorithm>
+// #include <algorithm>
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int main() {
+//     vector<int> A(3);
+//     for(int i = 0; i < 3; ++i) {
+//         cin >> A[i];
+//     }
+
+//     int b = A[1];
+//     sort(A.begin(), A.end());
+//     if(A[1] == b) {
+//         cout << "Yes" << endl;
+//     } else
+//         cout << "No" << endl;
+
+//     return 0;
+// }
+
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main() {
-    vector<int> A(3);
-    for(int i = 0; i < 3; ++i) {
-        cin >> A[i];
-    }
+    int a, b, c;
+    cin >> a >> b >> c;
 
-    int b = A[1];
-    sort(A.begin(), A.end());
-    if(A[1] == b) {
-        cout << "Yes" << endl;
+    if((a <= b && b <= c) || (a >= b && b >= c)) {
+        cout << "Yes"
+             << "\n";
     } else
-        cout << "No" << endl;
-
-    return 0;
+        cout << "No"
+             << "\n";
 }
