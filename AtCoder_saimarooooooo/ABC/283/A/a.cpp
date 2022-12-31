3c30b541e8e4ab3c536fbd6f7ea32d0cb8bec265
haruto17 <s1902272@sendai-nct.jp>
2022-12-31 16:20:56 +0900

    add abc283-a

---
+++ AtCoder_saimarooooooo/ABC/283/A/a.cpp
@@ -0,0 +1,13 @@
+// abc283-a
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << (int)pow(a, b) << "\n";
+    return 0;
+}
