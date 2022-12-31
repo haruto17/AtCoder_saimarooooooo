57f0ca8b8b0d1e327a1b129ccee23eaa3de3ccac
haruto17 <s1902272@sendai-nct.jp>
2022-12-31 16:11:20 +0900

    add abc279-a

---
+++ AtCoder_saimarooooooo/ABC/279/A/a.cpp
@@ -0,0 +1,23 @@
+// abc279-a
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int ans = 0;
+    for(int i = 0; i < s.size(); ++i) {
+        if(s[i] == 'v') {
+            ans += 1;
+        } else if(s[i] == 'w') {
+            ans += 2;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
